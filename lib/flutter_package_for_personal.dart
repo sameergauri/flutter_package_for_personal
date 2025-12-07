2743e1f91a37ac5ed8a10b122920ade446c4d337
Gauri Mohd Sameer <gaurimohdsameer@gmail.com>
2025-12-07 16:34:31 +0530

    update

--- lib/flutter_package_for_personal.dart
+++ lib/flutter_package_for_personal.dart
@@ -1,22 +1,18 @@
-library my_ui_widgets;
+library;
 
 // Export all buttons
 export 'src/buttons/custom_button.dart';
-
-// Export all textfields
-export 'src/textfields/custom_text_field_for_all.dart';
-
-// Export suggestion text field
-export 'src/textfields/custom_suggestion_text_field.dart';
-
-// Models
-export 'src/model/job_title_model.dart';
-
 // Enums
 export 'src/enum/enum.dart';
-
+// Export global constants
+export 'src/global/global.dart';
+// Models
+export 'src/model/job_title_model.dart';
 // Providers
 export 'src/provider/suggestion_text_field_provider.dart';
-
+// Export suggestion text field
+export 'src/textfields/custom_suggestion_text_field.dart';
+// Export all textfields
+export 'src/textfields/custom_text_field_for_all.dart';
 // Export utils
 export 'src/utils/colors.dart';
